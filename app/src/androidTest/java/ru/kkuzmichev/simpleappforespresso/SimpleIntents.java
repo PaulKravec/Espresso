c13e75c7a72e3b38c72cd546d0ddef0d745f2be3
Paul_KrVc <paul1312white@gmail.com>
2023-06-28 22:42:21 +0300

    Intents

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SimpleIntents.java
@@ -0,0 +1,62 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.openLinkWithUri;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SimpleIntents {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityScenarioRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void simpleIntents() {
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("This is home fragment")));
+
+        ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
+        overflowMenuButton.check(matches(isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction materialTextView = onView(withId(androidx.recyclerview.R.id.title));
+        materialTextView.check(matches(isDisplayed()));
+        materialTextView.perform(click());
+        Intents.intended(Matchers.allOf(
+                IntentMatchers.hasAction(Intent.ACTION_VIEW),
+                IntentMatchers.hasData( "https://google.com")));
+
+    }
+}
